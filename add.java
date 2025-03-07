e2bd9310f99e03021d27bb60e61d6a4127ccb273
Dhanalakshmialaguraj <rjdhana41107@gmail.com>
2025-03-07 12:02:21 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,9 +1,16 @@
-class add
-{
-public static void main(String args[])
-{
-int a=12,b=5,sum=0;
-sum=a+b;
-System.out.println("value of a and b is :" +sum);
-}
+class Add {
+    // Recursive method to calculate nth Fibonacci number
+    public static int fibonacci(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("n must be a positive integer");
+        }
+        if (n == 1) return 0;
+        if (n == 2) return 1;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int n = 10; // Change this value to compute different Fibonacci numbers
+        System.out.println("Fibonacci number at position " + n + " is: " + fibonacci(n));
+    }
 }
